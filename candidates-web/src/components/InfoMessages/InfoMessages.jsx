ea7c5ab2c1bb4a69c07200d33739eacd4afacbc3
Nicolás Logosettis <logo.ncls@gmail.com>
2023-11-08 00:45:31 -0300

    updates

--- candidates-web/src/components/InfoMessages/InfoMessages.jsx
+++ candidates-web/src/components/InfoMessages/InfoMessages.jsx
@@ -1,8 +1,7 @@
 // eslint-disable-next-line react/prop-types
-export const InfoMessages = ({ apiErrorMessage, megaverse, loading }) => (
+export const InfoMessages = ({ apiErrorMessage, loading }) => (
     <div data-testid="info-messages">
         {apiErrorMessage !== null && <span className='text-danger'>{apiErrorMessage}</span>}
-        {megaverse === null && !loading && !apiErrorMessage && <h4 className='text-warning'>An error ocurred</h4>}
         {loading && !apiErrorMessage && <h4 className='text-white'>Loading...</h4>}
     </div>
 )
